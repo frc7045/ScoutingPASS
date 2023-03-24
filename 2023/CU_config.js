2ef52e179da1db036c5d2035d6701c2fe51ebfc2
jsbondar <55658394+jsbondar@users.noreply.github.com>
2023-03-24 07:36:35 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023njwh",
+      "defaultValue": "2023njwas",
       "required": "true",
       "disabled": "true"
     },
